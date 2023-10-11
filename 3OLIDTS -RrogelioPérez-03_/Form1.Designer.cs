3c2b0db32d4a490e10c3e55ce52548ec4c0c0a39
CaptainRoger-rgb <144484861+CaptainRoger-rgb@users.noreply.github.com>
2023-10-10 20:32:59 -0500

    Agregar archivos de proyecto.

---
+++ 3OLIDTS -RrogelioPérez-03_/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace _3OLIDTS__RrogelioPérez_03_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCelsius = new System.Windows.Forms.Button();
+            this.btnFarenheit = new System.Windows.Forms.Button();
+            this.btnRestablecer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Location = new System.Drawing.Point(40, 54);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txtFahrenheit.TabIndex = 0;
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(40, 130);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Farenheit ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Celsius ";
+            // 
+            // btnCelsius
+            // 
+            this.btnCelsius.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCelsius.Location = new System.Drawing.Point(177, 54);
+            this.btnCelsius.Name = "btnCelsius";
+            this.btnCelsius.Size = new System.Drawing.Size(165, 32);
+            this.btnCelsius.TabIndex = 4;
+            this.btnCelsius.Text = "Convertir a Celsius ";
+            this.btnCelsius.UseVisualStyleBackColor = true;
+            this.btnCelsius.Click += new System.EventHandler(this.btnCelsius_Click);
+            // 
+            // btnFarenheit
+            // 
+            this.btnFarenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFarenheit.Location = new System.Drawing.Point(177, 123);
+            this.btnFarenheit.Name = "btnFarenheit";
+            this.btnFarenheit.Size = new System.Drawing.Size(165, 32);
+            this.btnFarenheit.TabIndex = 5;
+            this.btnFarenheit.Text = "Convertir a Farenheit ";
+            this.btnFarenheit.UseVisualStyleBackColor = true;
+            this.btnFarenheit.Click += new System.EventHandler(this.btnFarenheit_Click);
+            // 
+            // btnRestablecer
+            // 
+            this.btnRestablecer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestablecer.Location = new System.Drawing.Point(77, 183);
+            this.btnRestablecer.Name = "btnRestablecer";
+            this.btnRestablecer.Size = new System.Drawing.Size(165, 32);
+            this.btnRestablecer.TabIndex = 6;
+            this.btnRestablecer.Text = "Restablecer ";
+            this.btnRestablecer.UseVisualStyleBackColor = true;
+            this.btnRestablecer.Click += new System.EventHandler(this.btnRestablecer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRestablecer);
+            this.Controls.Add(this.btnFarenheit);
+            this.Controls.Add(this.btnCelsius);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCelsius;
+        private System.Windows.Forms.Button btnFarenheit;
+        private System.Windows.Forms.Button btnRestablecer;
+    }
+}
+
---
+++ 3OLIDTS -RrogelioPérez-03_/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3OLIDTS__RrogelioPérez_03_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCelsius_Click(object sender, EventArgs e)
+        {
+            float.TryParse(txtFahrenheit.Text, out float fahrenheit);
+            
+            float celsius = (fahrenheit - 32) * 5.0f / 9.0f;
+            txtCelsius.Clear();
+            txtCelsius.Text = celsius.ToString("0.00");
+        }
+
+        private void btnRestablecer_Click(object sender, EventArgs e)
+        {
+            txtFahrenheit.Text = "0.00";
+            txtCelsius.Text = "0.00";
+        }
+
+        private void btnFarenheit_Click(object sender, EventArgs e)
+        {
+            float.TryParse(txtCelsius.Text, out float celsius);
+
+            float fahrenheit = (celsius * 5.0f / 9.0f) + 32 ;
+            txtFahrenheit.Clear();
+            txtFahrenheit.Text = fahrenheit.ToString("0.00");
+        }
+    }
+}
